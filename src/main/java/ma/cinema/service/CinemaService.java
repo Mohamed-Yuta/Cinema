24c6eb7a8019ca13669c72fcfa453ac35de806f5
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-02-09 06:59:04 +0100

    service

--- src/main/java/ma/cinema/service/CinemaService.java
+++ src/main/java/ma/cinema/service/CinemaService.java
@@ -1,11 +1,10 @@
 package ma.cinema.service;
 
 import lombok.AllArgsConstructor;
-import ma.cinema.entity.Categorie;
-import ma.cinema.entity.Cinema;
-import ma.cinema.entity.Film;
-import ma.cinema.entity.Ville;
+import ma.cinema.entity.*;
 import ma.cinema.repository.*;
+
+import java.util.Date;
 
 @AllArgsConstructor
 public class CinemaService {
@@ -15,7 +14,10 @@ public class CinemaService {
     private final FilmRepository filmRepository ;
     private final CategorieRepository categorieRepository;
     private final PlaceRepository placeRepository ;
-
+    private final SalleRepository salleRepository;
+    private final SeanceRepository seanceRepository ;
+    private ProjectionFilmRepository projectionFilmRepository;
+    private final TicketRepository ticketRepository ;
     Ville saveVille(String name ) {
         Ville ville = new Ville();
         ville.setName(name);
@@ -46,6 +48,58 @@ public class CinemaService {
         categorieRepository.save(categorie);
         return filmRepository.save(film);
     }
+    Salle saveSalle(String name , String cinema , int nbr){
+        Cinema cinema1 = cinemaRepository.findByName(cinema);
+        Salle salle = new Salle();
+        salle.setCinema(cinema1);
+        salle.setNombrePlaces(nbr);
+        salle.setName(name);
+        cinema1.getSalles().add(salle);
+        cinemaRepository.save(cinema1);
+        return salleRepository.save(salle);
+    }
+    Seance saveSeance(Date heureDebut) {
 
+        Seance seance = new Seance();
+        seance.setHeureDebut(heureDebut);
+
+        return seanceRepository.save(seance);
+    }
+    Place savePlace(int numero, double latitude, double longitude, double altitude, Long salleId) {
+        Salle salle = salleRepository.findById(salleId).orElseThrow(() -> new RuntimeException("Salle not found"));
+
+        Place place = new Place();
+        place.setNumero(numero);
+        place.setLatitude(latitude);
+        place.setLongtitude(longitude);
+        place.setAtitude(altitude);
+        place.setSalle(salle);
+
+        salle.getPlaces().add(place);
+        salleRepository.save(salle);
+        return placeRepository.save(place);
+    }
+    ProjectionFim saveProjectionFim(double prix, Date dateProjection, Long salleId, Long filmId, Long seanceId) {
+        Salle salle = salleRepository.findById(salleId).orElseThrow(() -> new RuntimeException("Salle not found"));
+        Film film = filmRepository.findById(filmId).orElseThrow(() -> new RuntimeException("Film not found"));
+        Seance seance = seanceRepository.findById(seanceId).orElseThrow(() -> new RuntimeException("Seance not found"));
+
+        ProjectionFim projectionFim = new ProjectionFim();
+        projectionFim.setPrix(prix);
+        projectionFim.setDateProjection(dateProjection);
+        projectionFim.setSalle(salle);
+        projectionFim.setFilm(film);
+        projectionFim.setSeance(seance);
+
+        salle.getList().add(projectionFim);
+        film.getList().add(projectionFim);
+        seance.setProjectionFim(projectionFim);
+
+        salleRepository.save(salle);
+        filmRepository.save(film);
+        seanceRepository.save(seance);
+
+        return projectionFilmRepository.save(projectionFim);
+    }
 
 }
