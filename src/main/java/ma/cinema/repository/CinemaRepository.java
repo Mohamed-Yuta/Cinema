a2dcdc54e5b8d59877172b8829b58f566f469b33
Mohamed-Yuta <allali.mohamedamine22@gmail.com>
2024-02-09 07:07:47 +0100

    service

--- src/main/java/ma/cinema/repository/CinemaRepository.java
+++ src/main/java/ma/cinema/repository/CinemaRepository.java
@@ -1,7 +1,9 @@
 package ma.cinema.repository;
 
 import ma.cinema.entity.Cinema;
+import ma.cinema.entity.Ville;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CinemaRepository  extends JpaRepository<Cinema , Long> {
+    Cinema findByName(String name);
 }
